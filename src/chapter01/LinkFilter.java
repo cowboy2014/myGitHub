ce0e8cc23c3982dcbab11f9a369d3feb0544ae0a
cowboy2014 <714417743@qq.com>
2016-09-02 07:20:12 +0800

    chapter1
    队列处理、url抓取

---
+++ src/chapter01/LinkFilter.java
@@ -0,0 +1,8 @@
+package chapter01;
+
+/**
+ * Created by Administrator on 2016/8/28.
+ */
+public interface LinkFilter {
+    public boolean accept(String url);
+}
---
+++ src/chapter01/MyCrawler.java
@@ -0,0 +1,22 @@
+package chapter01;
+
+/**
+ * Created by Administrator on 2016/9/1.
+ */
+public class MyCrawler {
+    /**
+     * 使用种子初始化URL队列
+     * @param seeds
+     */
+    private void initCrawlerEithSeeds(String[] seeds){
+
+    }
+
+    /**
+     * 抓取过程
+     * @param seeds
+     */
+    public void crawling(String[] seeds){
+
+    }
+}
